edcce920343e458dfead1f0cdd2c269ad21b24d1
xpadro <xpadro.sw@gmail.com>
2014-01-17 14:36:18 +0100

    Inclusion of RestController annotation

--- spring-rest-api-v4/src/main/java/xpadro/spring/web/controller/SeriesController.java
+++ spring-rest-api-v4/src/main/java/xpadro/spring/web/controller/SeriesController.java
@@ -6,18 +6,17 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import xpadro.spring.web.model.Series;
 import xpadro.spring.web.service.SeriesService;
 
-@Controller
+@RestController
 @RequestMapping(value="/series")
 public class SeriesController {
 	
@@ -34,7 +33,6 @@ public class SeriesController {
 	 * @return
 	 */
 	@RequestMapping(method=RequestMethod.GET)
-	@ResponseBody
 	public Series[] getAllSeries() {
 		return seriesService.getAllSeries();
 	}
