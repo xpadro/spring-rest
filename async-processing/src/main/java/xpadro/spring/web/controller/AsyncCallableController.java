64314a8e16d4d760ae650ea42c30a6a91b9569dd
xpadro <xpadro.sw@gmail.com>
2015-07-12 12:19:47 +0200

    Implemented Callable controller

---
+++ async-processing/src/main/java/xpadro/spring/web/controller/AsyncCallableController.java
@@ -0,0 +1,31 @@
+package xpadro.spring.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import xpadro.spring.web.service.TaskService;
+
+import java.util.concurrent.Callable;
+
+@RestController
+public class AsyncCallableController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final TaskService taskService;
+
+    @Autowired
+    public AsyncCallableController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @RequestMapping(value = "/callable", method = RequestMethod.GET, produces = "text/html")
+    public Callable<String> executeSlowTask() {
+        logger.info("Request received");
+        Callable<String> callable = () -> taskService.execute();
+        logger.info("Servlet thread released");
+
+        return callable;
+    }
+}
